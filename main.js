38f7e7fa64109b247eadca57bd0a4afec69470af
Kemifrank <95023266+Kemifrank@users.noreply.github.com>
2022-04-06 16:32:15 -0700

    almost done

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 "use strict";
+window.addEventListener("load", () => {
+  let progressBar = document.querySelectorAll(".progress-bar");
+  let values = ["80%", "60%", "70%", "50%"];
+  progressBar.forEach((progress, index) => {
+    progress.style.width = values[index];
+  });
+});
+
+// const links = document.querySelectorAll(".navbar-nav nav-item nav-link");
+// for (const link of links) {
+//   link.addEventListener("click", smoothScroll);
+// }
+// function smoothScroll(e) {
+//   e.preventDefault();
+//   const href = this.getAttribute("href");
+//   document.querySelector(href).scrollIntoView({
+//     behaviour: "smooth",
+//   });
+// }
